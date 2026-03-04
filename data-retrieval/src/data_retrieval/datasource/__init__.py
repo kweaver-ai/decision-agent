9b1ad3b0f204389d67f55faf6b6f07659492489c
陈骁 (Chen, Xiao) <sh00tg0a1@live.com>
2026-03-04 12:27:01 +0800

    Delete data-retrieval directory (#69)

--- data-retrieval/src/data_retrieval/datasource/__init__.py
+++
@@ -1,7 +0,0 @@
-from data_retrieval.datasource.sqlite_ds import SQLiteDataSource
-from data_retrieval.datasource.vega_datasource import VegaDataSource
-
-__all__ = [
-    'SQLiteDataSource',
-    'VegaDataSource',
-]
--- data-retrieval/src/data_retrieval/prompts/__init__.py
+++
@@ -1,5 +0,0 @@
-from data_retrieval.prompts.tools_prompts.text2sql_prompt.text2sql import Text2SQLPrompt
-
-__all__ = [
-    "Text2SQLPrompt",
-]
